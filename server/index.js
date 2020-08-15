47df8ddc2dca543f298533288519bc906c0a4508
theunknownboy <theunknownride@gmail.com>
2020-08-15 12:23:45 +0530

    change prolem  response

--- server/index.js
+++ server/index.js
@@ -24,9 +24,7 @@ const connect = mongoose.connect(config.mongoURI,
   .catch(err => console.log(err));
 
 app.use(cors())
-app.get('/',(req,res) => {
-  res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
-})
+
 //to not get any deprecation warning or error
 //support parsing of application/x-www-form-urlencoded post data
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -44,14 +42,14 @@ app.use('/uploads', express.static('uploads'));
 
 // Serve static assets if in production
 if (process.env.NODE_ENV === "production") {
-
+  
   // Set static folder   
   // All the javascript and css files will be read and served from this folder
   app.use(express.static("client/build"));
 
-  // index.html for all page routes    html or routing and naviagtion
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
+  // index.html for all page routes html or routing and naviagtion
+  app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, "../client/build/", "index.html"));
   });
 }
 
